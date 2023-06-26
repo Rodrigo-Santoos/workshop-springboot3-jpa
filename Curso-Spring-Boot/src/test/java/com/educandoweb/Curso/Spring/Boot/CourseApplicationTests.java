b7614fe0e8df46663797b89bda053d993985d01b
Rodrigo Oliveira <47666631+Rodrigo-Santoos@users.noreply.github.com>
2023-06-26 15:24:15 -0300

    Projeto Criado

---
+++ Curso-Spring-Boot/src/test/java/com/educandoweb/Curso/Spring/Boot/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.Curso.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
