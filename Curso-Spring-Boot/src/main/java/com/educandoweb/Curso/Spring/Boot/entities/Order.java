ca31ae40b3cb351fa211d56229b6abd093f364dd
Rodrigo Oliveira <47666631+Rodrigo-Santoos@users.noreply.github.com>
2023-06-27 14:09:22 -0300

    OrderStatus enum

--- Curso-Spring-Boot/src/main/java/com/educandoweb/Curso/Spring/Boot/entities/Order.java
+++ Curso-Spring-Boot/src/main/java/com/educandoweb/Curso/Spring/Boot/entities/Order.java
@@ -4,8 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.Curso.Spring.Boot.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -30,6 +30,8 @@ public class Order implements Serializable{
 	
 	//associaçao com outra classe
 	
+	private Integer orderStatus;
+	
 	@ManyToOne //dizendo que é chave estrangeira no banco
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -41,10 +43,11 @@ public class Order implements Serializable{
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 	
@@ -67,6 +70,16 @@ public class Order implements Serializable{
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+	
 	public User getClient() {
 		return client;
 	}
